303416fabd4afecf2741d912ec1d5596c72ec89d
PRAKASH S <prakashs25698@gmail.com>
2020-04-16 12:26:23 +0530

    Updates

---
+++ precache-manifest.f2d0f2a97a003181cee8a81c7c6a1bee.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee73b0d6b2a28038e0803168c81b9c0f",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "bacce4abb77c3904b850",
+    "url": "/react-weather-app/static/css/2.e779567e.chunk.css"
+  },
+  {
+    "revision": "f11c71f06489cc624ea5",
+    "url": "/react-weather-app/static/css/main.1cdecf4d.chunk.css"
+  },
+  {
+    "revision": "bacce4abb77c3904b850",
+    "url": "/react-weather-app/static/js/2.9faa00cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.9faa00cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11c71f06489cc624ea5",
+    "url": "/react-weather-app/static/js/main.05c6654c.chunk.js"
+  },
+  {
+    "revision": "e664758ad626ae112e1f",
+    "url": "/react-weather-app/static/js/runtime-main.b06be747.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-weather-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/react-weather-app/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
